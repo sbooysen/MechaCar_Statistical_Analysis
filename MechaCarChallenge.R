3ee7f34aaa7fe2375b5ac2ab13f2a461bf157054
sbooysen <96075578+sbooysen@users.noreply.github.com>
2022-04-16 19:23:32 -0400

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,7 +1,42 @@
+# Deliverable 1:
+  
 library(dplyr)
 library(tidyverse)
-mecha_mpg <- read.csv(file='./Resources/MechaCar_mpg.csv',check.names=F,stringsAsFactors = F) 
-lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mecha_mpg)
-summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mecha_mpg)) 
-lm(mpg ~ vehicle_length + ground_clearance, data=mecha_mpg)
-summary(lm(mpg ~ vehicle_length + ground_clearance, data=mecha_mpg)) 
+
+mechacar_mpg <- read.csv(file='./Resources/MechaCar_mpg.csv',check.names=F,stringsAsFactors = F) 
+
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechacar_mpg)
+
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechacar_mpg)) 
+
+lm(mpg ~ vehicle_length + ground_clearance, data=mechacar_mpg)
+
+summary(lm(mpg ~ vehicle_length + ground_clearance, data=mechacar_mpg)) 
+
+# Deliverable 2:
+
+suspension_mecha <- read.csv(file='./Resources/Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+total_summary <- suspension_mecha %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI),Var_PSI=var(PSI), Std_Dev_PSI=sd(PSI), Num_Coil=n(), .groups = 'keep')
+
+lot_summary <- suspension_mecha  %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI),Median_PSI=median(PSI), Var_PSI=var(PSI), Std_Dev_PSI=sd(PSI), Num_Coil=n(), .groups = 'keep')
+
+#Deliverable 3
+
+plt1 <- ggplot(suspension_mecha,aes(y=PSI)) 
+
+plt1 + geom_boxplot() 
+
+plt2 <- ggplot(suspension_mecha,aes(x=Manufacturing_Lot,y=PSI)) 
+
+plt2 + geom_boxplot()
+
+#Deliverable 4
+
+t.test(suspension_mecha$PSI, mu=1500)
+
+t.test(subset(suspension_mecha,Manufacturing_Lot=="Lot1")$PSI, mu = 1500)
+
+t.test(subset(suspension_mecha,Manufacturing_Lot=="Lot2")$PSI, mu = 1500)
+
+t.test(subset(suspension_mecha,Manufacturing_Lot=="Lot3")$PSI, mu = 1500)
